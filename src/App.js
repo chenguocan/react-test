62683d8a54cd10a79497fc3e2f2c42426e189437
chenguocan <2622870670@qq.com>
2021-08-11 17:45:19 +0800

    '引入antd'

--- src/App.js
+++ src/App.js
@@ -1,70 +1,18 @@
+
 import React, { Component } from "react";
-import "./App.css"
-import Header from "./component/Header";
-import Main from "./component/Main";
-import Footer from "./component/Footer"
-import { nanoid } from "nanoid"
+import 'antd/dist/antd.css';
+import { Button } from "antd"
 class App extends Component {
-  state = {
-    todos: [{
-      id: nanoid(),
-      name: '吃饭',
-      done: true
-    }, {
-      id: nanoid(),
-      name: '睡觉',
-      done: false
-    }]
-  }
 
-  addTodo = (todoObj) => {
-    const { todos } = this.state
-    this.setState({
-      todos: [todoObj, ...todos]
-    })
-  }
-  //* todo赋值
-  updateTodo = (id) => {
-    const { todos } = this.state
-    const newObj = todos.map((todo) => {
-      if (todo.id === id) return { ...todo, done: !todo.done }
-      else return todo
-    })
-    this.setState({
-      todos: newObj
-    })
-  }
-
-  deleteTodo = (id) => {
-    console.log(id)
-    const { todos } = this.state
-    const newTodos = todos.filter((todo) => {
-      return todo.id !== id
-    })
-    this.setState({
-      todos: newTodos
-    })
-  }
-
-  handleDone = (flag) => {
-    console.log(flag)
-    const { todos } = this.state
-    const newObj=todos.map((todo) => {
-      return { ...todo, done: flag }
-    })
-    this.setState({
-      todos:newObj
-    })
-  }
   render() {
-    const { todos } = this.state
     return (
-      <div className="todo-container">
-        <div className="todo-wrap">
-          <Header addTodo={this.addTodo}></Header>
-          <Main todos={todos} updateTodo={this.updateTodo} deleteTodo={this.deleteTodo}></Main>
-          <Footer todos={todos} handleDone={this.handleDone}></Footer>
-        </div>
+      <div>
+        <Button type="primary">Primary Button</Button>
+        <Button>Default Button</Button>
+        <Button type="dashed">Dashed Button</Button>
+        <br />
+        <Button type="text">Text Button</Button>
+        <Button type="link">Link Button</Button>
       </div>
     )
   }
